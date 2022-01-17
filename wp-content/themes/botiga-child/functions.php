9dfed9f713e3cb964db665bd39e19fa08fcaa264
chaeeun <aug0901@naver.com>
2022-01-17 13:57:56 +0900

    우커머스에서 원화 기호의 위치와 표시 변경 함수 추가 (change_won_currency_symbol 함수 추가)

--- wp-content/themes/botiga-child/functions.php
+++ wp-content/themes/botiga-child/functions.php
@@ -18,3 +18,13 @@ function botiga_child_enqueue() {
 
 /* ADD YOUR CUSTOM FUNCTIONS BELOW */
 
+
+/* 우커머스에서 원화 기호의 위치와 표시 변경하기 (예 : 1,000원) */
+add_filter('woocommerce_currency_symbol', 'change_won_currency_symbol', 10, 2);
+
+function change_won_currency_symbol( $currency_symbol, $currency ) {
+    switch( $currency ) {
+        case 'KRW': $currency_symbol = '원'; break;
+    }
+    return $currency_symbol; }
+
