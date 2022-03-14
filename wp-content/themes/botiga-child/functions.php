1bb3e3a0670d3125a56abd904a5b59ce1e920031
chaeeun <aug0901@naver.com>
2022-03-14 17:22:35 +0900

    네이버 블로그로 공유하기 소스코드 추가

--- wp-content/themes/botiga-child/functions.php
+++ wp-content/themes/botiga-child/functions.php
@@ -73,6 +73,8 @@ function crunchify_social_sharing_buttons($content) {
         $crunchifyTitle = htmlspecialchars(urlencode(html_entity_decode(get_the_title(), ENT_COMPAT, 'UTF-8')), ENT_COMPAT, 'UTF-8');
         // $crunchifyTitle = str_replace( ' ', '%20', get_the_title());
 
+        $currentnavertitle = encodeURIComponent(get_the_title());   // 네이버 블로그 공유하기 추가
+
         // Get Post Thumbnail for pinterest
         $crunchifyThumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
 
@@ -82,6 +84,7 @@ function crunchify_social_sharing_buttons($content) {
         $googleURL = 'https://plus.google.com/share?url='.$crunchifyURL;
         $bufferURL = 'https://bufferapp.com/add?url='.$crunchifyURL.'&amp;text='.$crunchifyTitle;
         $linkedInURL = 'https://www.linkedin.com/shareArticle?mini=true&url='.$crunchifyURL.'&amp;title='.$crunchifyTitle;
+        $naverURL = 'http://blog.naver.com/openapi/share?url='.$crunchifyURL.'&amp;title='.$currentnavertitle;   // 네이버 블로그 추가
 
         // Based on popular demand added Pinterest too
         $pinterestURL = 'https://pinterest.com/pin/create/button/?url='.$crunchifyURL.'&amp;media='.$crunchifyThumbnail[0].'&amp;description='.$crunchifyTitle;
@@ -92,6 +95,7 @@ function crunchify_social_sharing_buttons($content) {
         $content .= '<h5>SNS로 공유하기</h5> <a class="crunchify-link crunchify-twitter" href="'. $twitterURL .'" target="_blank">Twitter</a>';
         $content .= '<a class="crunchify-link crunchify-facebook" href="'.$facebookURL.'" target="_blank">Facebook</a>';
         $content .= '<a class="crunchify-link crunchify-googleplus" href="'.$googleURL.'" target="_blank">Google+</a>';
+        $content .= '<a class="crunchify-link crunchify-naverblog" href="'.$naverURL.'" target="_blank">네이버블로그</a>';
         //$content .= '<a class="crunchify-link crunchify-buffer" href="'.$bufferURL.'" target="_blank">Buffer</a>';
         //$content .= '<a class="crunchify-link crunchify-linkedin" href="'.$linkedInURL.'" target="_blank">LinkedIn</a>';
         //$content .= '<a class="crunchify-link crunchify-pinterest" href="'.$pinterestURL.'" data-pin-custom="true" target="_blank">Pin It</a>';
@@ -104,3 +108,10 @@ function crunchify_social_sharing_buttons($content) {
     }
 };
 add_filter( 'the_content', 'crunchify_social_sharing_buttons');
+
+// 네이버 블로그 공유하기 추가
+function encodeURIComponent($str) {
+    $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
+    return strtr(rawurlencode($str), $revert);
+}
+// Reference: stackoverflow
